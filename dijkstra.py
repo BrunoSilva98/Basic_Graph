014e296ccb4abf50238590888b303b91e3c83047
BrunoSilva98 <brunops98@hotmail.com>
2019-11-24 17:38:31 -0300

    adicao de funcoes auxiliares

--- dijkstra.py
+++ dijkstra.py
@@ -5,6 +5,7 @@ class Dijsktra:
         self.destino = no_destino
         self.lista_distancia_acumulada = list()
         self.graph = grafo.copy()
+        self.menor_caminho = list()
 
     def inicializa_lista(self):
         for vertice in self.graph.vertices:
@@ -15,5 +16,27 @@ class Dijsktra:
             if (vertice.vertice == self.origem.vertice):
                 vertice.distancia_acumulada = 0
     
+    def get_menor_vertice(self):
+        menor_dist = float('inf')
+        for vertice in self.lista_distancia_acumulada:
+            if (vertice.expandido == False):
+                if (vertice.distancia_acumulada < menor_dist):
+                    menor_dist = vertice
+        return menor_dist
+
+    def atualiza_lista(self, anterior, atual, distancia_acumulada):
+        for vertex in self.lista_distancia_acumulada:
+            if(vertex.vertice == atual.vertice):
+                atual.anterior = anterior
+                atual.distancia_acumulada = distancia_acumulada
+
     def dijsktra_algorithm(self):
-        pass
+        self.inicializa_lista()
+        vertex = self.origem
+        while(vertex.vertice != self.destino.vertice):
+            vertex = self.get_menor_vertice()
+            for adjacente in vertex.adjacentes:
+                distancia_acumulada = vertex.distancia_acumulada + adjacente.peso
+                if(distancia_acumulada < adjacente.distancia_acumulada):
+                    self.atualiza_lista(vertex, adjacente, distancia_acumulada)
+            vertex.expandido = True

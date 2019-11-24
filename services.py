5921d0707f1db287d017832a779b33bb252aeb80
BrunoSilva98 <brunops98@hotmail.com>
2019-11-24 12:23:21 -0300

    adicao da verificacao de arvore minima e selecao de menor aresta"

--- services.py
+++ services.py
@@ -3,5 +3,34 @@ import no
 
 def arvore_geradora_minima_prim(graph):
     arv_minima = grafo.Grafo()
-    arv_minima.addVertice(graph.vertices[0])
+    corte = grafo.Grafo()
+    corte.addVertice(graph.vertices[0])
+
+    while not verifica_arv_minima(graph, arv_minima):
+
+
+def seleciona_menor_aresta(graph):
+    menor_peso = float('inf')
     
+    for vertice in graph.vertices:
+        for aresta in vertice.adjacentes:
+            if (graph.getVertice(aresta.vertice) == None):
+                if (aresta.peso < menor_peso):
+                    menor_peso = aresta
+    return menor_peso
+
+def verifica_arv_minima(graph, arv):
+    lista_vertices_grafo = get_all_vertices(graph)
+    lista_vertices_arv = get_all_vertices(arv)
+
+    for vertice in lista_vertices_grafo:
+        if vertice not in lista_vertices_arv:
+            return False
+    return True
+
+def get_all_vertices(graph):
+    lista_vertices = list()
+    for v in graph.vertices:
+        lista_vertices.append(v.vertice)
+    return lista_vertices
+        
